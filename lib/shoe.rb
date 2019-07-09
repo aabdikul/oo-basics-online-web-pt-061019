55fee21cbad3ccef65687ce848239f3efec7402c
Aida Abdikulova <abdikulova@gmail.com>
2019-07-09 04:26:23 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,4 +2,11 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
+
+  attr_accessor :color
+  attr_accessor :size
+  attr_accessor :material
+  attr_accessor :condition
+  attr_reader :brand
+
 end
